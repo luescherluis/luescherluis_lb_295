cd6c52cb70e0830726d3f51dec9f036d67caddd9
Luis Meister » <l.lscher.inf21>
2023-07-09 08:10:53 +0200

    Add essential models (Spieler + Fussballmannschaft)

---
+++ FootballManagement/Model/SpielerDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace FootballManagement.Model
+{
+    public class SpielerDTO
+    {
+        public int Id { get; set; }
+
+        public string? Nachname { get; set; }
+
+        public string? Vorname { get; set; }
+
+    }
+}
